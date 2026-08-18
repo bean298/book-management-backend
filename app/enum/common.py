78c81560e93429e7026021584e1434a3653de9d8
Tuanbin <tuanbin2982003@gmail.com>
2026-08-18 15:57:30 +0700

    add new enum for order model

--- app/enum/common.py
+++ app/enum/common.py
@@ -15,3 +15,17 @@ class UserRole(str, Enum):
 class ResetMethod(str, Enum):
     OTP = "otp"
     LINK = "link"
+
+
+class OrderStatus(str, Enum):
+    PENDING = "pending"
+    CONFIRMED = "confirmed"
+    SHIPPED = "shipped"
+    DELIVERED = "delivered"
+    CANCELLED = "cancelled"
+
+
+class PaymentMethod(str, Enum):
+    CASH = "cash"
+    CREDIT = "bank_transfer"
+    MOMO = "momo"
